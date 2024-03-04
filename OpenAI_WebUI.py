57facafceee057e23e7275e9f37b985707a39141
MFHSchool <119766714+MFHSchool@users.noreply.github.com>
2024-03-04 08:44:10 +0100

    Update OpenAI_WebUI.py

--- OpenAI_WebUI.py
+++ OpenAI_WebUI.py
@@ -43,7 +43,7 @@ def main():
     if cf.isBlank(save_location):
         st.error(f"Could not find the OAIWUI_SAVEDIR environment variable")
         cf.error_exit("Could not find the OAIWUI_SAVEDIR environment variable")
-    save_location = os.path.join(os.path.dirname(os.path.realpath(sys.path[0])), save_location.strip("/"))
+    save_location = os.path.join(os.path.dirname(os.path.realpath(sys.path[0])), save_location)
     err = cf.check_existing_dir_w(save_location, "OAIWUI_SAVEDIR directory")
     if cf.isNotBlank(err):
         st.error(f"While ching OAIWUI_SAVEDIR: {err}")
